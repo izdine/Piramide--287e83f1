6f8f60b818cdffb41b3df2aa0a1b6d8dd941156f
Iz-Dine Amadou <2088346@talnet.nl>
2019-11-27 14:06:53 +0100

    Uitwerking van de exercise

---
+++ piramide.php
@@ -0,0 +1,13 @@
+<?php
+echo "Hoeveel stapels wil je hebben?:\n";
+$e = readline ();
+forgit add -A && git commit -am "Uitwerking van de exercise" && git push ($i=1; $i < $e; $i++) {
+  for ($j= 1; $j <= $i; $j++) {
+
+    echo "*"."*";
+  }
+echo ''. PHP_EOL;
+
+}
+
+ ?>
